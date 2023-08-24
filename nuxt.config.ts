966a18799286eeecdc912771509dd0fa46ff74ca
Mert Tekin <32420405+mertttekin@users.noreply.github.com>
2023-08-24 15:54:20 +0300

    Co-authored-by: Emre Ensar Çapcı <emreensr@users.noreply.github.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['nuxt-swiper'],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   devtools: { enabled: true }
 })
