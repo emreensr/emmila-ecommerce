8eadacbd5f5cf583295f7a9dd31a87915df15d58
emreensr <emreensar.capci@gmail.com>
2023-08-28 21:31:06 +0300

    some modifications product detail page

--- pages/[category]/[product].vue
+++ pages/[category]/[product].vue
@@ -1,63 +1,13 @@
 <template>
     <div>
-        <div class="grid grid-cols-1 lg:grid-cols-6 gap-5 mx-auto max-w-8xl lg:pt-20 pt:12 px-3">
-            <div class="col-span-1">
-                <client-only>
-                    <Swiper
-                        class="vertical-swiper"
-                        :modules="[
-                            SwiperAutoplay,
-                            SwiperEffectCreative,
-                            SwiperNavigation,
-                        ]"
-                        :slides-per-view="1"
-                        :space-between="0"
-                        :direction="'vertical'"
-                        :breakpoints="{
-                            640: {
-                            slidesPerView: 1,
-                            },
-                            768: {
-                            slidesPerView: 1,
-                            },
-                            1024: {
-                            slidesPerView: 1,
-                            },
-                        }"
-                        :loop="true"
-                        :autoplay="{ delay: 5000, disableOnInteraction: false }"
-                        :navigation="{
-                            clickable: true,
-                            nextEl: '.swiper-button-next',
-                            prevEl: '.swiper-button-prev',
-                        }"
-                        >
-                        <swiper-slide v-if="!isMobile" class="relative" v-for="photo in productImages" :key="photo.id">
-                            <img class="w-full object-cover" :src="photo.img" />
-                        </swiper-slide>
-                        <div class="swiper-button-next">
-                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" color="#fff"
-                                 xmlns="http://www.w3.org/2000/svg" style="color: rgb(255, 255, 255)">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
-                                    d="M9 13.887l5-5V8.18l-5-5-.707.707 4.146 4.147H2v1h10.44L8.292 13.18l.707.707z"></path>
-                            </svg>
-                        </div>
-                        <div class="swiper-button-prev">
-                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" color="#fff"
-                             xmlns="http://www.w3.org/2000/svg" style="color: rgb(255, 255, 255)">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
-                                    d="M7 3.093l-5 5V8.8l5 5 .707-.707-4.146-4.147H14v-1H3.56L7.708 3.8 7 3.093z"></path>
-                            </svg>
-                        </div>
-                    </Swiper>
-                </client-only>
-            </div>
-            <div class="col-span-3">
+        <div class="grid grid-cols-1 lg:grid-cols-7 gap-5 mx-auto max-w-8xl lg:pt-12 mb-24 px-3">
+            <div class="col-span-4">
                 <client-only>
                     <Swiper
                         :modules="[
                             SwiperAutoplay,
                             SwiperEffectCreative,
+                            SwiperPagination,
                             SwiperNavigation,
                         ]"
                         :slides-per-view="1"
@@ -74,13 +24,18 @@
                         }"
                         :loop="true"
                         :autoplay="{ delay: 5000, disableOnInteraction: false }"
+                        :pagination="{
+                            clickable: true,
+                            bulletClass: 'swiper-bullet',
+                            bulletActiveClass: 'swiper-bullet-active',
+                          }"
                         :navigation="{
                             clickable: true,
                             nextEl: '.swiper-button-next',
                             prevEl: '.swiper-button-prev',
                         }"
                         >
-                        <swiper-slide v-if="!isMobile" class="relative" v-for="photo in productImages" :key="photo.id">
+                        <swiper-slide class="relative" v-for="photo in productImages" :key="photo.id">
                             <img class="h-custom w-full object-cover" :src="photo.img" />
                         </swiper-slide>
                         <div class="swiper-button-next">
@@ -100,7 +55,7 @@
                     </Swiper>
                 </client-only>
             </div>
-            <div class="col-span-2">
+            <div class="col-span-3">
                 <div class="flex justify-between">
                     <div>
                         <h3 class="font-bold text-md">EMMILA</h3>
@@ -121,14 +76,16 @@
                 <div class="mt-5">
                     <span class="font-bold text-lg mt-5">1250₺</span>
                 </div>
-                <div class="flex mt-5">
-                    <div class="flex flex-1 space-x-8 text-xl items-center">
+                <div class="flex mt-5 gap-3">
+                    <div class="flex space-x-8 text-xl items-center">
                         <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em"
+                            class="cursor-pointer"
                             width="1em" xmlns="http://www.w3.org/2000/svg">
                             <path d="M5 11h14v2H5z"></path>
                         </svg>
                         <span class="text-md">1</span>
                         <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em"
+                            class="cursor-pointer"
                             width="1em" xmlns="http://www.w3.org/2000/svg">
                             <path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path>
                         </svg>
@@ -140,10 +97,10 @@
                 <div>
                     <button class="bg-black p-3 w-full text-white font-bold mt-3">HEMEN AL</button>
                 </div>
-                <div class="border-t border-gray-200 py-6 transform transition-all duration-300">
-                    <h3 class="-mx-2 -my-3 flow-root">
+                <div class="border-t border-gray-200 mt-6 bg-[#f8f8f8] transform transition-all duration-300">
+                    <h3 class="mx-1">
                         <button type="button" @click="toggleDescription"
-                            class="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500"
+                            class="flex w-full items-center justify-between px-2 py-3 text-gray-400 hover:text-gray-500"
                             aria-controls="filter-section-mobile-0" aria-expanded="false">
                             <span class="font-medium text-gray-900 hover:underline hover:underline-offset-1">Ürün
                                 Açıklaması</span>
@@ -163,7 +120,7 @@
                         <div v-if="showDescription" class="pt-5 overflow-y-hidden" id="filter-section-mobile-0">
                             <div class="space-y-4">
                                 <div class="flex items-center">
-                                    <p class="text-sm">Pels Basic Pouch, Pels Side Pocket Crossbody iPhone kılıfınız için mükemmel bir
+                                    <p class="text-sm p-3">Pels Basic Pouch, Pels Side Pocket Crossbody iPhone kılıfınız için mükemmel bir
                                         eklentidir. Pasaportunuz, anahtarınız, makyaj malzemeniz, not defteriniz ve çok daha
                                         fazlası için sahip olunması gereken bir el çantasıdır.<br> Halkaları sayesinde Pels Side
                                         Pocket Crossbody iPhone kılıfınıza kolayca eklenebilir. İster tek başına elinizde ya
@@ -192,7 +149,7 @@
 </template>
 
 <script setup>
-const showDescription = ref(false)
+const showDescription = ref(true)
 
 const toggleDescription = () => {
     showDescription.value = !showDescription.value;
@@ -201,20 +158,36 @@ const toggleDescription = () => {
 const productImages = ref([
     {
         id: 1,
-        img: "https://cdn.myikas.com/images/c92faa73-5650-4267-9e11-9aac520a912b/abe2a1c7-1893-44a1-93fd-d36a65cf633e/image_540.webp",
+        img: "https://cdn.myikas.com/images/c92faa73-5650-4267-9e11-9aac520a912b/564e51ad-fd90-4d5e-abbe-aefef170dd7f/image_1728.webp",
     },
     {
         id: 2,
-        img: "https://cdn.myikas.com/images/c92faa73-5650-4267-9e11-9aac520a912b/abe2a1c7-1893-44a1-93fd-d36a65cf633e/image_540.webp",
+        img: "https://cdn.myikas.com/images/c92faa73-5650-4267-9e11-9aac520a912b/bc6b9245-4baf-45d2-9e84-6a3a6c825564/image_1728.webp",
     },
     {
         id: 3,
-        img: "https://cdn.myikas.com/images/c92faa73-5650-4267-9e11-9aac520a912b/abe2a1c7-1893-44a1-93fd-d36a65cf633e/image_540.webp",
+        img: "https://cdn.myikas.com/images/c92faa73-5650-4267-9e11-9aac520a912b/cfe1fb7a-75ba-4169-9938-9f2f67b6108c/image_1728.webp",
+    },
+    {
+        id: 4,
+        img: "https://cdn.myikas.com/images/c92faa73-5650-4267-9e11-9aac520a912b/e9757358-57d2-42bc-9ed8-9a00cdd358c8/image_1728.webp",
     },
 ]);
 </script>
 
 <style lang="scss">
+.swiper-pagination-bullets {
+    @apply flex justify-center lg:pb-12 space-x-4 items-center w-full;
+  
+    .swiper-bullet {
+      @apply bg-white h-2 w-2 cursor-pointer rounded-full mt-10;
+    }
+  
+    .swiper-bullet-active {
+      @apply bg-[#010C20] lg:w-10 w-7;
+    }
+  }
+  
 .swiper-button-prev {
     left: 10px !important;
     background-repeat: no-repeat;
@@ -251,17 +224,17 @@ const productImages = ref([
     }
 }
 
-.vertical-swiper {
-  display: flex;
-  flex-direction: column; 
-  align-items: center;
+@media only screen and (min-width: 767px) {
+    .swiper-pagination-bullets {
+        display: none;
+    }
+
+    .swiper-bullet {
+        display: none;
+    }
 }
 
-.vertical-swiper img {
-  max-height: 100%;
-  width: auto;
-  margin: auto;
-}
+
 
 .h-custom {
     max-height: calc(100vh - 125px);
